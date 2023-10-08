7698544996b777c209799554dff2033f891842fe
Alex Kruglov <lime4512@mail.ru>
2023-10-08 16:20:35 +0300

    build 0.5

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
 const ApiKey = 'd178825f29659a9ed7a31647'
 
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
-const text = document.querySelector('.text')
+async function wellData() {
+	const response = await fetch(
+		`https://v6.exchangerate-api.com/v6/${ApiKey}/latest/USD`
+	)
+	const data = await response.json()
+	document.querySelector('.RUB-T').innerHTML = Math.round(
+		data.conversion_rates.RUB
+	)
+	document.querySelector('.CZK-T').innerHTML = Math.round(
+		data.conversion_rates.CZK
+	)
+	document.querySelector('.EUR-T').innerHTML = Math.round(
+		data.conversion_rates.EUR
+	)
+}
+
+wellData()
+
+const giveInput = document.querySelector('.Give')
+const receiveInput = document.querySelector('.Receive')
+
+const giveSelect = document.querySelector('.Give-name')
+const receiveSelect = document.querySelector('.Receive-name')
 
 async function fetchData() {
 	const response = await fetch(
-		`https://v6.exchangerate-api.com/v6/${ApiKey}/latest/RUB`
+		`https://v6.exchangerate-api.com/v6/${ApiKey}/latest/${giveSelect.value}`
 	)
 	const data = await response.json()
-	text.textContent = `${(
-		Number(input.value) * data.conversion_rates.USD
-	).toFixed(2)} долларов`
+	if (receiveSelect.value == 'USD') {
+		receiveInput.value = (giveInput.value * data.conversion_rates.USD).toFixed(
+			2
+		)
+	} else if (receiveSelect.value == 'RUB') {
+		console.log(data.conversion_rates.RUB)
+		receiveInput.value = (giveInput.value * data.conversion_rates.RUB).toFixed(
+			2
+		)
+	} else if (receiveSelect.value == 'EUR') {
+		receiveInput.value = (giveInput.value * data.conversion_rates.EUR).toFixed(
+			2
+		)
+	}
 }
 
-btn.addEventListener('click', function () {
+giveInput.addEventListener('blur', function () {
 	fetchData()
 })
+
+//Сделать разного цвета курс валют, оптимизировать код, добавить парочку новых валют, добавить кнопку быстрого свапа валют
